039788af5a594dc8ecadb6c7feb64525f0da02f4
kexplx <oscar.rosner@web.de>
2019-10-27 21:21:31 +0100

    adds two dataprovider classes implementing IDataProvider

---
+++ app/Data/ODSDataProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * First sentence is a short description. Then you can write more, just as you like.
+ *
+ * Here may follow some detailed description about what the class is for.
+ *
+ * Paragraphs are separated by an empty line.
+ */
+class ODSDataProvider implements IDataProvider
+{
+    /**
+     * A description for this method.
+     * 
+     * @param string $var Description.
+     *
+     * @return string Description.
+     */
+    public function getCountyCrimeRate(string $countyName): float
+    {
+        return 0.0;
+    }
+
+    /**
+     * A description for this method.
+     * 
+     * @param string $var Description.
+     *
+     * @return string Description.
+     */
+    public function getCountiesOnRoute(float $from, float $to): array
+    {
+        return [];
+    }
+}
