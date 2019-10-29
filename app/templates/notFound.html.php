ecd3f91d7cd471403c6a2cc7718c2df193653493
kexplx <oscar.rosner@web.de>
2019-10-29 23:04:05 +0100

    adds not found controller and page for invalid requests

---
+++ app/templates/notFound.html.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <title>404 / CrimeView</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
+    You're lost buddy.
+</body>
+</html>
