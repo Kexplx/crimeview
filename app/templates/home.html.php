e7235d74a4228785cb9bc6f3eec5375838830e85
kexplx <oscar.rosner@web.de>
2019-10-29 23:05:05 +0100

    changes title

--- app/templates/home.html.php
+++ app/templates/home.html.php
@@ -1,11 +1,14 @@
 <html lang="en">
+
 <head>
+    <title>Home / CrimeView</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
 </head>
+
 <body>
     <div></div>
 </body>
+
 </html>
