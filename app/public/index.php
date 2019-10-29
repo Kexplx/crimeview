379e88609627b8dfd1022058edd88054606f3bee
kexplx <oscar.rosner@web.de>
2019-10-29 23:03:37 +0100

    makes index.php to be single point of entry for all requests

--- app/public/index.php
+++ app/public/index.php
@@ -1,8 +1,40 @@
-<?php 
+<?php
 
-ini_set('display_errors', 'on');
+use PharIo\Manifest\InvalidUrlException;
 
-require __DIR__ . '/../../vendor/autoload.php';
-require __DIR__ . '/../Logging.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../Logging.php';
+
+$c = ucfirst($_GET['c']);
+$a = $_GET['a'];
+
+try {
+    validateParams($c, $a);
+    $controllerName = $c . 'Controller';
+
+    $controller = new $controllerName();
+    $controller->$a();
+} catch (InvalidUrlException $e) { 
+    $log->error($e);
+    // Code
+} catch (Throwable $th) {
+    $log->error($th);
+}
 
 
+function validateParams(string $c, string $a)
+{
+    if (!isset($c) || !isset($a)) {
+        redirect('Home', 'serve');
+    }
+
+    if (!class_exists($c . 'Controller')) {
+        $msg = sprintf('Controller %s not found.', "$c");
+        throw new InvalidUrlException($msg);
+    }
+
+    if (!method_exists($c . 'Controller', $a)) {
+        $msg = sprintf('Action %s not defined in Controller %sController.', $a, $c);
+        throw new InvalidUrlException($msg);
+    }
+}
