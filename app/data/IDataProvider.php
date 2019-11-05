dd69f40c4b870f39e625d6ef1d172bfc48ebf0d1
kexplx <oscar.rosner@web.de>
2019-11-05 01:28:00 +0100

    moves method doc up to interface level

--- app/data/IDataProvider.php
+++ app/data/IDataProvider.php
@@ -1,35 +1,35 @@
 <?php
 
 /**
- * Represents an IDataProvider class used to fetch county and city data from a specific source.
+ * Represents a IDataProvider used to fetch county and city data
  */
 interface IDataProvider
 {
     /**
-     * Get CrimeRate for a countyname.
+     * Gets the latest crime rate of a county
      * 
-     * @param string $countyName.
+     * @param string $countyName Name of the county
      *
-     * @return float amount of crimes per x inhabitants
+     * @return float The countie's crime rate (amount of crimes / inhabitants)
      */
     public function getCountyCrimeRate(string $countyName): float;
 
     /**
-     * Get all Counties on a Route.
+     * Gets all counties on a route
      * 
-     * @param City $from.
-     * @param City $to.
+     * @param City $from The departure city
+     * @param City $to The destination city
      *
-     * @return array of Counties on given route.
+     * @return array Counties on given route
      */
     public function getCountiesOnRoute(City $from, City $to): array;
 
     /**
-     * Get City Model from Name.
+     * Gets City by name
      * 
-     * @param string $name Cityname.
+     * @param string $name The city name to look up
      *
-     * @return City Citymodel.
+     * @return City The city model
      */
     public function getCityFromName(string $name): City;
 }
