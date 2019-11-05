5a636992586c207aef29a3411521e9c3a86a3d05
kexplx <oscar.rosner@web.de>
2019-11-05 02:50:48 +0100

    starts unit test implementation for getCounties

--- tests/controllers/HomeControllerTest.php
+++ tests/controllers/HomeControllerTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+
 /**
  * Provides unit testing for app/controllers/HomeController
  */
@@ -12,13 +13,34 @@ class HomeControllerTest extends TestCase
      */
     public function testServe()
     {
-        $expected = file_get_contents( __DIR__ . "/../../app/templates/home.html.php");
-        
+        $expected = file_get_contents(__DIR__ . "/../../app/templates/home.html.php");
+
         ob_start();
-        $homeController = new HomeController();
+        $homeController = new HomeController(new MockDataProvider());
         $homeController->serve();
         $actual = ob_get_clean();
 
         $this->assertSame($expected, $actual);
+    }
+
+    public function testGetCounties()
+    {
+        require_once __DIR__ . '/../../vendor/autoload.php';
+        ini_set("display_errors", "on");
+        $c = new HomeController(new MockDataProvider());
+
+        $_GET["from"] = "regensburg";
+        $_GET["to"] = "erlangen";
+        $c->getCounties();
+        ob_start();
+
+        $response = ob_get_clean();
+
+        echo $response;
+
+        json_decode($response);
+        if (json_last_error() != JSON_ERROR_NONE) {
+            $this->fail('getCounties() didn\'t return valid json');
+        }
     }
 }
