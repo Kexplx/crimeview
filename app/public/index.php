b656655c9e7215fbe827997fc09998d854a0dc08
kexplx <oscar.rosner@web.de>
2019-11-21 00:25:30 +0100

    catches invalidargumentexception and changes response code accordingly

--- app/public/index.php
+++ app/public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use PharIo\Manifest\InvalidUrlException;
+
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../Logging.php';
 
@@ -13,10 +15,13 @@ try {
 
     $controller = new $controllerName(new OriginDataProvider());
     $controller->$a();
-} catch (InvalidArgumentException $e) {
+} catch (InvalidUrlException $e) {
     $log->error($e);
     $controller = new NotFoundController();
     $controller->serve();
+} catch (InvalidArgumentException $e) {
+    $log->error($e);
+    http_response_code(404);
 } catch (Throwable $th) {
     $log->error($th);
 }
@@ -30,11 +35,11 @@ function validateParams(string $c = null, string $a = null)
 
     if (!class_exists($c . 'Controller')) {
         $msg = sprintf('Controller %s not found.', "$c");
-        throw new InvalidArgumentException($msg);
+        throw new InvalidUrlException($msg);
     }
 
     if (!method_exists($c . 'Controller', $a)) {
         $msg = sprintf('Action %s not defined in Controller %sController.', $a, $c);
-        throw new InvalidArgumentException($msg);
+        throw new InvalidUrlException($msg);
     }
 }
