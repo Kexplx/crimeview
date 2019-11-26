21e42f6dcbdea319541dd039625e1a3bf9b778e8
kexplx <oscar.rosner@web.de>
2019-11-26 18:40:02 +0100

    uses new router class to handle requests and input validation

--- app/public/index.php
+++ app/public/index.php
@@ -1,47 +1,7 @@
 <?php
 
-use PharIo\Manifest\InvalidUrlException;
+require __DIR__ . '/../../vendor/autoload.php';
+$routes = require "routes.php";
 
-require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/../Logging.php';
-
-$c = ucfirst(htmlspecialchars($_GET['c']));
-$a = htmlspecialchars($_GET['a']);
-
-try {
-    validateParams($c, $a);
-    $controllerName = $c . 'Controller';
-
-    (new $controllerName(new CrimeViewDataProvider(
-        new OriginDataProvider(),
-        new OriginDataProvider(),
-        new OriginDataProvider()
-    )))->$a();
-} catch (InvalidUrlException $e) {
-    $log->error($e);
-    $controller = new NotFoundController();
-    $controller->serve();
-} catch (InvalidArgumentException $e) {
-    $log->error($e);
-    http_response_code(404);
-} catch (Throwable $th) {
-    $log->error($th);
-}
-
-function validateParams(string $c = null, string $a = null)
-{
-    if ($c == "" || $a == "") {
-        header("Location: ?c=Home&a=serve");
-        exit;
-    }
-
-    if (!class_exists($c . 'Controller')) {
-        $msg = sprintf('Controller %s not found.', "$c");
-        throw new InvalidUrlException($msg);
-    }
-
-    if (!method_exists($c . 'Controller', $a)) {
-        $msg = sprintf('Action %s not defined in Controller %sController.', $a, $c);
-        throw new InvalidUrlException($msg);
-    }
-}
+$router = new CrimeViewRouter($routes);
+$router->direct(Request::url());
