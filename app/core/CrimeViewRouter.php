5b4198e9b122ce0a14f2cb76f29666c9713e3fed
kexplx <oscar.rosner@web.de>
2019-11-29 04:03:09 +0100

    injects SampleDataProvider instead of OriginDataProvider into CrimeViewDataProvider

--- app/core/CrimeViewRouter.php
+++ app/core/CrimeViewRouter.php
@@ -50,7 +50,7 @@ class CrimeViewRouter
         if (method_exists($controller, $action)) {
             return (new $controller(
                 new CrimeViewDataProvider(
-                    new OriginDataProvider(),
+                    new SampleDataProvider(),
                     new OriginDataProvider(),
                     new OriginDataProvider()
                 )
