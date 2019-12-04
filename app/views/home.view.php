2327829b99e1b9a49faf9a4851174a0d01084fe2
kexplx <oscar.rosner@web.de>
2019-12-04 14:09:21 +0100

    removes openLayer.js

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -9,7 +9,6 @@
     <script src="assets/js/vendor/jquery-3.4.1.min.js"></script>
     <script src="assets/js/vendor/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/places.js@1.17.0"></script>
-    <script src="http://www.openlayers.org/api/OpenLayers.js"></script>
     <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js" integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og==" crossorigin=""></script>
     <script src="assets/js/vendor/leaflet-providers.js"></script>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
