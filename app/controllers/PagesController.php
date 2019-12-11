717922028d9e6124abf345bf3e09a63223b8b1dd
kexplx <oscar.rosner@web.de>
2019-12-11 13:28:52 +0100

    removes hits counter from display and controller

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -12,17 +12,7 @@ class PagesController
 
     public function home()
     {
-        $path = __DIR__ . '/hits';
-
-        if (file_exists($path)) {
-            $hits = intval(file_get_contents($path));
-        } else {
-            $hits = 1;
-            file_put_contents($path, '1');
-        }
-
-        file_put_contents($path, ++$hits);
-        view('home', ["hits" => $hits]);
+        view('home');
     }
 
     public function notFound()
