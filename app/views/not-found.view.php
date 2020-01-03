740a4bd99eacf3e700d25647f7f6882087204004
kexplx <oscar.rosner@web.de>
2020-01-03 06:19:27 +0100

    improves layout for 720px layout and beyond

--- app/views/not-found.view.php
+++ app/views/not-found.view.php
@@ -6,28 +6,26 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>404 / CrimeView</title>
     <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
-<body class="bg-shape">
-    <section class="hero-banner" style="margin-top: 100px;">
-        <div class="container">
-            <div class="row align-items-center align-items-xl-center justify-content-between text-center text-md-left">
-                <div class="col-md-6 col-lg-5 mb-5 mb-md-0">
-                    <h1>You're lost buddy.</h1>
-                    <p>It's important to stay save while traveling.
-                        <br>
-                        Thats why you should use CrimeCiew :)
-                    </p>
-                    <button onclick="location.href='/home'" type="link" class="btn btn-dark btn-sm">Beam me home</button>
-                </div>
-                <div class="col-md-8 col-lg-5">
-                    <img class="main-img" src="assets/images/not-found.svg" alt="Not found image">
-                </div>
+<body>
+    <div class="container mt-5">
+        <h1>
+            You're lost buddy
+            <img style="width: 40px" src="assets/images/not-found.svg" alt="Not found image">
+        </h1>
+        <hr>
+        <div class="row">
+            <div class="col-12">
+                <p>It's important to stay save while traveling.
+                    Thats why you should use CrimeCiew :)
+                </p>
+                <button onclick="location.href='/home'" type="link" class="btn btn-dark">Beam me home</button>
             </div>
         </div>
-    </section>
+    </div>
 </body>
 
 </html>
