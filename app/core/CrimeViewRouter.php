4ff94c9a178bcb9065dc8d43f6baeee625f36529
kexplx <oscar.rosner@web.de>
2020-01-10 15:30:38 +0100

    uses mapquestapiprovider as default city provider

--- app/core/CrimeViewRouter.php
+++ app/core/CrimeViewRouter.php
@@ -51,7 +51,7 @@ class CrimeViewRouter
             return (new $controller(
                 new CrimeViewDataProvider(
                     new LocalDataProvider(),
-                    new OriginDataProvider(),
+                    new MapQuestApiDataProvider(),
                     new OriginDataProvider()
                 )
             ))->$action();
