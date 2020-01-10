4d226c445aea3e89d19ef69e2e80f5209249984f
kexplx <oscar.rosner@web.de>
2020-01-10 10:27:23 +0100

    adds mapQuestApiDataProvider as an alternative to nominatim geocoding

---
+++ app/data/providers/MapQuestApiDataProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * MapQuestApiProvider uses MapQuestApi to retrieve city information.
+ */
+class MapQuestApiDataProvider implements ICityDataProvider
+{
+    // $url = http://www.mapquestapi.com/geocoding/v1/address?key=43v5BKEkPRheVl5GC7OptE5UDsyihftJ&maxResults=1&location=Erlangen
+
+    // $body = file_get_contents($url);
+    // $json = json_decode($body);
+    // 
+    // $status = $json->info->statuscode; 
+    // $lat = $json->results[0]->locations[0]->latLng->lat;
+    // $lng = $json->results[0]->locations[0]->latLng->lng;
+}
