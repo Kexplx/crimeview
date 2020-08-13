091043d832d0d2e9aa647d91171323624db21a77
Oscar rosner <oscar.rosner@web.de>
2020-08-13 14:23:34 +0200

    feat: add map component

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss'],
+})
+export class MapComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
