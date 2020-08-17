b789753b97e27878b23043ec64812e215cb25b0d
Oscar Rosner <oscar.rosner@web.de>
2020-08-17 04:00:49 +0200

    feat: change form

--- src/app/city/city-list/city-list.component.ts
+++ src/app/city/city-list/city-list.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit, InjectionToken } from '@angular/core';
-import { CityService } from '../city.service';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { City } from '../city.service';
 
 @Component({
   selector: 'app-city-list',
   templateUrl: './city-list.component.html',
   styleUrls: ['./city-list.component.scss'],
 })
-export class CityListComponent implements OnInit {
-  constructor() {}
+export class CityListComponent {
+  cities: City[] = [];
 
-  ngOnInit(): void {}
+  onCitySelect(city: City): void {
+    const isDistinct = !this.cities.find(c => c.placeId === city.placeId);
+
+    if (isDistinct) {
+      this.cities.push(city);
+    }
+  }
 }
