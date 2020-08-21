6a78cf373b78da27b87ff6793605d1d510e9b2ee
Oscar Rosner <oscar.rosner@web.de>
2020-08-21 08:04:32 +0200

    feat: remove commonModule from imports

--- src/app/county/county.module.ts
+++ src/app/county/county.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { COUNTY_CRIME_RATES } from './county-crimerates';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
   providers: [{ provide: 'COUNTY_CRIME_RATES', useValue: COUNTY_CRIME_RATES }],
 })
 export class CountyModule {}
