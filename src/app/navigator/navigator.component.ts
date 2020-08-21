8a34289e943360fdd07f4c001af022678e7b99e1
Oscar Rosner <oscar.rosner@web.de>
2020-08-21 21:08:44 +0200

    feat: remove unused constructor and ngOnInit hook

--- src/app/navigator/navigator.component.ts
+++ src/app/navigator/navigator.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { City } from '../city/models/city';
+import { CountyService } from '../county/county.service';
+import { Subject, Observable } from 'rxjs';
+import { County } from '../county/models/county';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navigator',
   templateUrl: './navigator.component.html',
   styleUrls: ['./navigator.component.scss'],
 })
-export class NavigatorComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class NavigatorComponent {}
