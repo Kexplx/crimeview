5161950f338c9041fb611d70e847957341896574
Oscar Rosner <oscar.rosner@web.de>
2020-08-21 21:00:12 +0200

    fix: reformat imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, InjectionToken } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
-import { NavigatorComponent } from './navigator/navigator.component';
-import { CityModule } from './city/city.module';
-import { MapComponent } from './map/map.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavigatorModule } from './navigator/navigator.module';
 import { CountyModule } from './county/county.module';
+import { MapModule } from './map/map.module';
+import { CityModule } from './city/city.module';
 
 @NgModule({
-  declarations: [AppComponent, MapComponent, NavigatorComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, SharedModule, CityModule, HttpClientModule, NavigatorModule, CountyModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    CityModule,
+    HttpClientModule,
+    NavigatorModule,
+    CountyModule,
+    MapModule,
+  ],
 
   bootstrap: [AppComponent],
 })
