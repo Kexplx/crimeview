f4d7581cbf7052de3bf27857badd37539f2e6b8e
Oscar Rosner <oscar.rosner@web.de>
2020-09-22 00:45:55 +0200

    feat: remove multiplication of crimerate by 100k through pipe or component

--- src/app/search-details/search-details.module.ts
+++ src/app/search-details/search-details.module.ts
@@ -4,18 +4,10 @@ import { SharedModule } from '../shared/shared.module';
 import { CityNamesPipe } from './city-names.pipe';
 import { CountyTableComponent } from './county-table/county-table.component';
 import { TypePipe } from './county-table/type.pipe';
-import { CrimeRatePipe } from './county-table/crime-rate.pipe';
 import { StatePipe } from './county-table/state.pipe';
 
 @NgModule({
-  declarations: [
-    SearchDetailsComponent,
-    CityNamesPipe,
-    CountyTableComponent,
-    TypePipe,
-    CrimeRatePipe,
-    StatePipe,
-  ],
+  declarations: [SearchDetailsComponent, CityNamesPipe, CountyTableComponent, TypePipe, StatePipe],
   imports: [SharedModule],
   exports: [SearchDetailsComponent],
 })
