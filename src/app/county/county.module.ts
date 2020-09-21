d7c1ba0d9f091ba6cbb2af5c9067d0f417410f05
Oscar Rosner <oscar.rosner@web.de>
2020-09-22 00:42:47 +0200

    refactor: remove county crimerates from providers

--- src/app/county/county.module.ts
+++ src/app/county/county.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { COUNTY_CRIME_RATES } from './county-crimerates';
 import { CountyService } from './county.service';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [SharedModule],
-  providers: [CountyService, { provide: 'COUNTY_CRIME_RATES', useValue: COUNTY_CRIME_RATES }],
+  providers: [CountyService],
 })
 export class CountyModule {}
