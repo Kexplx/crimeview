a0c7d97ef9a528b9af6d65cbbd36d14bd18b3560
Oscar Rosner <oscar.rosner@web.de>
2020-09-22 16:01:23 +0200

    refactor: add type for county type

--- src/app/county/interfaces/county.ts
+++ src/app/county/interfaces/county.ts
@@ -1,12 +1,14 @@
 import { Geometry } from 'geojson';
 
+export type CountyType = 'Landkreis' | 'Kreis' | 'Kreisfreie Stadt';
+
 /**
  * The structure of a county.
  */
 export interface County {
   name: string;
   state: string;
-  type: string;
+  type: CountyType;
   countyCode: string;
   crimeRate: number | undefined;
   geometry: Geometry;
--- src/app/search-details/county-table/type.pipe.ts
+++ src/app/search-details/county-table/type.pipe.ts
@@ -1,10 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { CountyType } from 'src/app/county/interfaces/county';
 
 @Pipe({
   name: 'type',
 })
 export class TypePipe implements PipeTransform {
-  transform(type: 'Landkreis' | 'Kreisfreihe Stadt'): 'LK' | 'KFS' {
-    return type === 'Landkreis' ? 'LK' : 'KFS';
+  private typeAbbreviations = new Map<string, string>([
+    ['Landkreis', 'LK'],
+    ['Kreis', 'K'],
+    ['Kreisfreie Stadt', 'KfS'],
+  ]);
+
+  transform(type: CountyType): string {
+    const abbr = this.typeAbbreviations.get(type);
+
+    return abbr ?? type;
   }
 }
