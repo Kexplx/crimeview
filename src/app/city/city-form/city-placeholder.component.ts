4d148a153689065238db32fa50c9bc6d061a45db
Oscar Rosner <oscar.rosner@web.de>
2020-09-22 22:33:29 +0200

    feat: add placeholders for cities

---
+++ src/app/city/city-form/city-placeholder.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-city-placeholder',
+  template: `
+    <div class="wrapper">
+      <div class="wrapper__checkbox-hint"></div>
+      <div class="wrapper__text-hint"></div>
+    </div>
+  `,
+  styles: [
+    `
+      .wrapper {
+        display: grid;
+        grid-template-columns: 1fr 6fr;
+        grid-template-rows: 17px;
+        gap: 0 5px;
+        margin-top: 3px;
+      }
+
+      .wrapper div {
+        background-color: #0000001a;
+        border-radius: 2px;
+      }
+
+      .wrapper__text-hint {
+        margin: 1px 0;
+      }
+    `,
+  ],
+})
+export class CityPlaceholderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/city/city.module.ts
+++ src/app/city/city.module.ts
@@ -5,9 +5,16 @@ import { CityService } from './city.service';
 import { CityInputComponent } from './city-input/city-input.component';
 import { PredictionPipe } from './city-input/prediction.pipe';
 import { IsCheckedPipe } from './city-form/is-checked.pipe';
+import { CityPlaceholderComponent } from './city-form/city-placeholder.component';
 
 @NgModule({
-  declarations: [CityInputComponent, CityFormComponent, PredictionPipe, IsCheckedPipe],
+  declarations: [
+    CityInputComponent,
+    CityFormComponent,
+    PredictionPipe,
+    IsCheckedPipe,
+    CityPlaceholderComponent,
+  ],
   imports: [SharedModule],
   providers: [CityService, google.maps.Geocoder, google.maps.places.AutocompleteService],
   exports: [CityFormComponent],
