bf1793a892a0f45cf7249e421c68fd780e700a97
Oscar Rosner <oscar.rosner@web.de>
2020-09-25 02:57:42 +0200

    feat: forkJoin districts

--- src/app/district/district.service.ts
+++ src/app/district/district.service.ts
@@ -1,17 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { City } from '../city/interfaces/city';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { forkJoin, Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 import { District } from './interfaces/district';
 import { OsmDistrict } from './interfaces/osm-district';
-import { COUNTY_CRIME_RATES } from './district-crimerates';
-
-// prettier-ignore
-const OSM_BASE_API = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=georef-germany-kreis&rows=403&fields=krs_code,krs_name_short,krs_type,lan_name,geo_shape&';
-const OSM_RADIUS_API = OSM_BASE_API + 'geofilter.distance=';
-const OSM_POLYGON_API = OSM_BASE_API + 'geofilter.polygon=';
-const OSM_LINE_API = OSM_BASE_API + 'geofilter.polygon=';
+import { environment } from '../../environments/environment';
 
 enum SearchTypes {
   Radius = 1,
@@ -19,48 +13,44 @@ enum SearchTypes {
   Polygon = 3,
 }
 
-export interface OsmResponse {
-  records: { fields: OsmDistrict }[];
-}
+type OsmResponse = { records: { fields: OsmDistrict }[] };
+
+const { Line, Polygon, Radius } = SearchTypes;
+const { districtsByLine, districtsByPolygon, districtsByRadius } = environment.urls.openstreetmap;
 
 @Injectable()
 export class DistrictService {
   constructor(private http: HttpClient) {}
 
   getCounties(cities: City[]): Observable<District[]> {
-    return this.getOsmCounties(cities).pipe(
-      map(osmCounties => {
-        return osmCounties.map<District>(
-          ({ krs_code, krs_name_short, krs_type, lan_name, geo_shape }) => ({
-            districtCode: krs_code,
-            name: krs_name_short,
-            type: krs_type,
-            state: lan_name,
-            geometry: geo_shape,
-            crimeRate: COUNTY_CRIME_RATES.get(krs_code),
-          }),
-        );
-      }),
+    return this.getOsmDistricts(cities).pipe(
+      switchMap(osmDistricts =>
+        forkJoin(
+          osmDistricts.map<Observable<District>>(({ krs_code, lan_name, geo_shape }) =>
+            this.http
+              .get<District>(environment.urls.countyById, { params: { code: krs_code } })
+              .pipe(map(distrct => ({ ...distrct, geometry: geo_shape, stateName: lan_name }))),
+          ),
+        ),
+      ),
     );
   }
 
-  private getOsmCounties(cities: City[]): Observable<OsmDistrict[]> {
+  private getOsmDistricts(cities: City[]): Observable<OsmDistrict[]> {
     const url = this.buildUrl(cities, cities.length);
 
     return this.http.get<OsmResponse>(url).pipe(map(({ records }) => records.map(r => r.fields)));
   }
 
   private buildUrl(cities: City[], type: SearchTypes): string {
-    const { Line, Polygon, Radius } = SearchTypes;
-
     // prettier-ignore
     switch (type) {
       case Line:
-        return `${OSM_LINE_API}(${cities[0].lat},${cities[0].lng}),(${cities[1].lat},${cities[1].lng}),(${cities[1].lat},${cities[1].lng + 0.000001})`;
+        return `${districtsByLine}(${cities[0].lat},${cities[0].lng}),(${cities[1].lat},${cities[1].lng}),(${cities[1].lat},${cities[1].lng + 0.000001})`;
       case Polygon:
-        return `${OSM_POLYGON_API}(${cities[0].lat},${cities[0].lng}),(${cities[1].lat},${cities[1].lng}),(${cities[2].lat},${cities[2].lng})`;
+        return `${districtsByPolygon}(${cities[0].lat},${cities[0].lng}),(${cities[1].lat},${cities[1].lng}),(${cities[2].lat},${cities[2].lng})`;
       case Radius:
-        return `${OSM_RADIUS_API}${cities[0].lat},${cities[0].lng},10000`;
+        return `${districtsByRadius}${cities[0].lat},${cities[0].lng},10000`;
     }
   }
 }
