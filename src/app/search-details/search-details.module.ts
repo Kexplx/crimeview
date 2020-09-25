0e772e3a1aa42cd753d0e035bf345a9e2c3c01e6
Oscar Rosner <oscar.rosner@web.de>
2020-09-25 02:56:27 +0200

    refactor: remove type pipe

--- src/app/search-details/search-details.module.ts
+++ src/app/search-details/search-details.module.ts
@@ -3,11 +3,9 @@ import { SearchDetailsComponent } from './search-details.component';
 import { SharedModule } from '../shared/shared.module';
 import { CityNamesPipe } from './city-names.pipe';
 import { DistrictTableComponent } from './district-table/district-table.component';
-import { TypePipe } from './district-table/type.pipe';
-import { StatePipe } from './district-table/state.pipe';
 
 @NgModule({
-  declarations: [SearchDetailsComponent, CityNamesPipe, DistrictTableComponent, TypePipe, StatePipe],
+  declarations: [SearchDetailsComponent, CityNamesPipe, DistrictTableComponent],
   imports: [SharedModule],
   exports: [SearchDetailsComponent],
 })
