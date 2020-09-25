d99e679d8adf2be699f8fac85abe548771b76dd5
Oscar Rosner <oscar.rosner@web.de>
2020-09-25 02:57:15 +0200

    feat: add properties to district model

--- src/app/district/interfaces/district.ts
+++ src/app/district/interfaces/district.ts
@@ -3,13 +3,22 @@ import { Geometry } from 'geojson';
 export type DistrictType = 'Landkreis' | 'Kreis' | 'Kreisfreie Stadt';
 
 /**
- * The structure of a district.
+ * Represents a german district.
  */
 export interface District {
+  code: number; // A unique identifier with 5 characters.
   name: string;
-  state: string;
   type: DistrictType;
-  districtCode: string;
-  crimeRate: number | undefined;
+  population: number;
+  crestUrl: string; // The url of the districts's crest as svg.
+  stateName: string; // The federal state containing the district.
+
+  totalOffencesCount: number;
+  relativeOffencesCount: number; // Number of offences per 100.000 inhabitants.
+  commonOffences: { offenceName: string; offencesCount: number }[];
+  totalSuspectsCount: number;
+  nonGermanSuspectsCount: number; //
+  solvedCasesCount: number;
+
   geometry: Geometry;
 }
