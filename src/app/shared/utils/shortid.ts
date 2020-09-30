fe7fbffc886f2e81d9c6952702765ccbc0a90af3
Oscar Rosner <oscar.rosner@web.de>
2020-10-01 01:21:40 +0200

    remove: imports and helpers

--- src/app/shared/utils/shortid.ts
+++
@@ -1,6 +0,0 @@
-const SET = 'abcdefghijklmnopqrstuvwxyz';
-const LENGTH = 10;
-
-export function shortid(): string {
-  return [...Array(LENGTH)].map(() => SET[Math.floor(Math.random() * SET.length)]).join('');
-}
