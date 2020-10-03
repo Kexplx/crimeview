7730466cd45dbe394adda59c7218c74b89f4bc7e
thomasRiecher93 <70125329+thomasRiecher93@users.noreply.github.com>
2020-10-03 05:06:06 +0200

    fix(search-details): restructure map callback

--- src/app/search-details/clicked-district-details/values.pipe.ts
+++ src/app/search-details/clicked-district-details/values.pipe.ts
@@ -4,10 +4,14 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'values',
 })
 export class ValuesPipe implements PipeTransform {
-  transform(value: { offenceName: string; offencesCount: number }[]): unknown {
-    return value.map<{ name: string; value: number }>(co => ({
-      name: co.offenceName,
-      value: co.offencesCount,
-    }));
+  transform(
+    commonOffences: { offenceName: string; offencesCount: number }[],
+  ): { name: string; value: number }[] {
+    return commonOffences.map<{ name: string; value: number }>(
+      ({ offenceName: name, offencesCount: value }) => ({
+        name,
+        value,
+      }),
+    );
   }
 }
