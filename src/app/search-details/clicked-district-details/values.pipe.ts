ab6f6e9f0f5d1bbf59191b45130e649515f38ef1
thomasRiecher93 <70125329+thomasRiecher93@users.noreply.github.com>
2020-10-03 04:55:48 +0200

    fix(search-details): replace unknown with actual return type

--- src/app/search-details/clicked-district-details/values.pipe.ts
+++ src/app/search-details/clicked-district-details/values.pipe.ts
@@ -4,7 +4,9 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'values',
 })
 export class ValuesPipe implements PipeTransform {
-  transform(value: { offenceName: string; offencesCount: number }[]): unknown {
+  transform(
+    value: { offenceName: string; offencesCount: number }[],
+  ): { name: string; value: number }[] {
     return value.map<{ name: string; value: number }>(co => ({
       name: co.offenceName,
       value: co.offencesCount,
