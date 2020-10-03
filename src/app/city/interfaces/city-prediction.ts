7d21c9afbeb2f5e5e25ae37b47bb8d6249cb67a2
Oscar Rosner <oscar.rosner@web.de>
2020-10-03 05:10:04 +0200

    docs: add docs to city predictions

--- src/app/city/interfaces/city-prediction.ts
+++ src/app/city/interfaces/city-prediction.ts
@@ -1,3 +1,8 @@
+/**
+ * Represents the prediction of a city, based on previous user input.
+ *
+ * "Reg" -> {placeId: "0129", name: "Regensburg"};
+ */
 export interface CityPrediction {
   placeId: string;
   name: string;
