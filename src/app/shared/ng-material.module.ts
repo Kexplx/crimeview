7a4b746520d9100dd725858c2878b82ff7ce4d4b
Oscar Rosner <oscar.rosner@web.de>
2020-10-10 08:50:08 +0200

    feat: export chips and progressbarmodule

--- src/app/shared/ng-material.module.ts
+++ src/app/shared/ng-material.module.ts
@@ -9,14 +9,18 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
   exports: [
     MatCheckboxModule,
+    MatChipsModule,
     MatButtonModule,
     MatInputModule,
     MatAutocompleteModule,
     MatFormFieldModule,
+    MatProgressBarModule,
     MatExpansionModule,
     MatSnackBarModule,
     MatTableModule,
